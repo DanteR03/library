cb1f17e32f0d40849658e8a99d939011f8e48634
Lev <danteru.kim@gmail.com>
2022-04-25 20:10:53 +0500

    Change and update function responsible for displaying books to execute everytime after adding new book. Make it create buttons

--- library.js
+++ library.js
@@ -14,10 +14,12 @@ function addBookToLibrary() {
   let status = window.prompt("Status");
   let newBook = new Book(title, author, pages, status);
   myLibrary.push(newBook);
+  displayBooks(myLibrary);
 }
 
 function displayBooks(array) {
   for (let book of array) {
+    if (book.displayed === false || book.displayed === undefined) {
     const table = document.querySelector("table");
     const row = document.createElement("tr");
     const titleCell = document.createElement("td");
@@ -28,8 +30,19 @@ function displayBooks(array) {
     pagesCell.textContent = book.pages;
     const statusCell = document.createElement("td");
     statusCell.textContent = book.status;
-    row.append(titleCell, authorCell, pagesCell, statusCell);
+    const buttons = document.createElement("td");
+    buttons.classList.add("buttons");
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.textContent = "Delete";
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit");
+    editButton.textContent = "Edit";
+    buttons.append(deleteButton, editButton);
+    row.append(titleCell, authorCell, pagesCell, statusCell, buttons);
     table.appendChild(row);
+    book.displayed = true;
+    };
   };
 }
 
