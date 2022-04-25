aa2312420e6b7883c5dd73370dd5cb96610a1d76
Lev <danteru.kim@gmail.com>
2022-04-25 17:30:41 +0500

    Write a function to display books in table

--- library.js
+++ library.js
@@ -15,3 +15,20 @@ function addBookToLibrary() {
   let newBook = new Book(title, author, pages, status);
   myLibrary.push(newBook);
 }
+
+function displayBooks(array) {
+  array.forEach(function(book) {
+    const table = document.querySelector("table");
+    const row = document.createElement("tr");
+    const titleCell = document.createElement("td");
+    titleCell.textContent = book.title;
+    const authorCell = document.createElement("td");
+    authorCell.textContent = book.author;
+    const pagesCell = document.createElement("td");
+    pagesCell.textContent = book.pages;
+    const statusCell = document.createElement("td");
+    statusCell.textContent = book.status;
+    row.append(titleCell, authorCell, pagesCell, statusCell);
+    table.appendChild(row);
+  });
+}
