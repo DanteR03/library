d2f2b6558f20d7c93f606e88bbd7ab4ec2b830e6
Lev <101640837+DanteR03@users.noreply.github.com.>
2024-12-05 02:48:50 +0500

    Create function that displays books from the library array on the page

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ function addBookToLibrary(title, author, pages, status) {
     let book = new Book(title, author, pages, status);
     myLibrary.push(book);
 }
+
+function displayLibraryBooks() {
+    const booksContainer = document.querySelector(".books-container");
+    booksContainer.innerHTML = "";
+    myLibrary.forEach((book) => {
+        const bookElement = document.createElement("div");
+        const titlePara = document.createElement("p");
+        const authorPara = document.createElement("p");
+        const pagesPara = document.createElement("p");
+        const statusPara = document.createElement("p");
+        const buttonContainer = document.createElement("div");
+        const deleteButton = document.createElement("button");
+        const changeButton = document.createElement("button");
+        titlePara.textContent = book.title;
+        authorPara.textContent = book.author;
+        pagesPara.textContent = book.pages;
+        statusPara.textContent = book.status;
+        deleteButton.textContent = "Delete";
+        changeButton.textContent = "Change Status";
+        buttonContainer.append(deleteButton, changeButton);
+        buttonContainer.classList.add("book-buttons");
+        bookElement.append(titlePara, authorPara, pagesPara, statusPara, buttonContainer);
+        bookElement.classList.add("book");
+        booksContainer.appendChild(bookElement);
+    })
+}
