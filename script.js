9d53f7cf60df7e48e1bbf0a59ad2546226368960
Lev <101640837+DanteR03@users.noreply.github.com.>
2024-12-05 03:53:57 +0500

    Add a modal that allows user to input book data and add the book to the library

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 const myLibrary = [];
+const newBookBtn = document.querySelector(".header-container > button");
+const newBookModal = document.querySelector(".book-form-modal");
+const modalCloseButton = document.querySelector(".form-close-button");
+const modalSubmitButton = document.querySelector(".form-submit-button");
+
+newBookBtn.addEventListener("click", () => {
+    newBookModal.showModal();
+});
+
+modalCloseButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    newBookModal.close();
+});
+
+modalSubmitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const bookForm = document.querySelector(".book-form-modal > form");
+    const formTitleInput = document.querySelector("input#title");
+    const formAuthorInput = document.querySelector("input#author");
+    const formPagesInput = document.querySelector("input#pages");
+    const formStatusInput = document.querySelector("input#status");
+    if (bookForm.checkValidity() === false) {
+        bookForm.reportValidity();
+    } else if (formStatusInput.checked === true) {
+        addBookToLibrary(formTitleInput.value, formAuthorInput.value, formPagesInput.value, "Read");
+        displayLibraryBooks();
+        newBookModal.close();
+    } else {
+        addBookToLibrary(formTitleInput.value, formAuthorInput.value, formPagesInput.value, "Not Read");
+        displayLibraryBooks();
+        newBookModal.close();
+    }
+})
 
 function Book(title, author, pages, status) {
     this.title = title;
